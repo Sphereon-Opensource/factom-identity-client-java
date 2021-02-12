7ea57babaac427bbca34e43564c9d1e71fffc7b5
Scott Malley <srmalley-sphereon@github.com>
2021-02-12 16:10:55 +0100

    remove setting of DID document context as it is set automatically

--- src/main/java/com/sphereon/factom/identity/did/DIDConstants.java
+++ src/main/java/com/sphereon/factom/identity/did/DIDConstants.java
@@ -1,7 +1,6 @@
 package com.sphereon.factom.identity.did;
 
 public class DIDConstants {
-    public static final String JSONLD_DID_DOCUMENT_CONTEXT = "https://www.w3.org/ns/did/v1";
     public static final String JSONLD_TERM_ID = "id";
     public static final String JSONLD_TERM_TYPE = "type";
     public static final String  JSONLD_TERM_CONTROLLER = "controller";
